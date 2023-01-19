a02ffeb1b2c8002744a5fb891358d8e0897a5346
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2023-01-19 13:04:09 +0100

    Add project files.

---
+++ Deconstruction_Example/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deconstruction_Example
+{
+    public class Person
+    {
+        internal string Name { get; set; }
+        internal int BirthYear { get; set; }
+        internal void Deconstruct(out string name, out int birthYear)
+        {
+            name = Name;
+            birthYear = BirthYear;
+        }
+    }
+}
---
+++ Deconstruction_Example/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deconstruction_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var pat = new Person()
+            {
+                Name = "Patrick",
+                BirthYear = 1990
+            };
+
+            var (name, birthYear) = pat;
+            Console.WriteLine($"Name: {name}, year of birth: {birthYear}");
+            Console.ReadKey();  
+        }
+    }
+}
